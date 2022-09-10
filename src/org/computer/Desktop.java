d8c809e1b822a710538b578c29f07a3f113350cd
kavi123 <kavyaharshiraj@gmail.com>
2022-09-10 09:32:42 +0530

    initial commit

---
+++ src/org/computer/Desktop.java
@@ -0,0 +1,20 @@
+package org.computer;
+
+public class Desktop implements HardWare,Software {
+
+	@Override
+	public void softwareResources() {
+	System.out.println("Intel");	
+	}
+
+	@Override
+	public void harewareResorces() {
+	System.out.println("Laptop");	
+	}
+	public static void main(String[] args) {
+		Desktop d = new Desktop();
+		d.softwareResources();
+		d.harewareResorces();
+	}
+
+}
